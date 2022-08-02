9d8b73bcacbdc7cee790e4f09876eb7c4cb3d3d2
Siwon <73284904+tldnjs324@users.noreply.github.com>
2022-08-03 02:41:10 +0900

    LoginScene

--- Assets/Scripts/login/Login.cs
+++ Assets/Scripts/login/Login.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Login : MonoBehaviour
 {
+
+    public GameObject loginCanvas;
+    public GameObject registerCanvas;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +19,25 @@ public class Login : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void GoRegister()
+    {
+        loginCanvas.SetActive(false);
+        registerCanvas.SetActive(true);
+    }
+    public void GoLogin()
+    {
+        loginCanvas.SetActive(true);
+        registerCanvas.SetActive(false);
+    }
+    public void JoinSucceed()
+    {
+        loginCanvas.SetActive(true);
+        registerCanvas.SetActive(false);
+    }
+    public void LoginSucceed()
+    {
+        SceneManager.LoadScene("YardScene");
     }
 }
