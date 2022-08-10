18f677392cb21b28b7f62067f74d28fbd37a78c6
Siwon <73284904+tldnjs324@users.noreply.github.com>
2022-08-10 15:56:32 +0900

    비번 일치 확인

--- Assets/Scripts/login/Login.cs
+++ Assets/Scripts/login/Login.cs
@@ -3,22 +3,62 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class Login : MonoBehaviour
 {
 
     public GameObject loginCanvas;
     public GameObject registerCanvas;
+
+    //public TextMeshProUGUI pw;
+    //public TextMeshProUGUI pwre;
+    public Text pw;
+    public Text pwre;
+    public Text pwNotice;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //pw = GetComponent<TextMeshProUGUI>();
+        //pwre = GetComponent<TextMeshProUGUI>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       
+        if (pwre.text != "")
+        {
+            if (pw.text == pwre.text)
+            {
+                pwNotice.text = "비밀번호가 일치합니다";
+            }
+            else
+            {
+                pwNotice.text = "비밀번호가 일치하지 않습니다";
+            }
+        }
+        else
+        {
+            pwNotice.text = "";
+        }
+        /*
+        if (pwre == null)
+        {
+            pwNotice.text = "";
+        }
+        else
+        {
+            if (pw.text == pwre.text)
+            {
+                pwNotice.text = "비밀번호가 일치합니다";
+            }
+            else
+            {
+                pwNotice.text = "비밀번호가 일치하지 않습니다";
+            }
+        }*/
     }
 
     public void GoRegister()
